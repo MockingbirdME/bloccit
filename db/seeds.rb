b051c662c0fb0bf39273995d7f92e00874ffd84c
Devon Henegar <devon.henegar@gmail.com>
2016-06-13 09:05:18 -0400

    add idempotent post and comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,9 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+Post.find_or_create_by(title: "This post is always here", body: "Always here, always here; no, it's not going anywhere")
+post = Post.find_by(title:"This post is always here")
+Comment.find_or_create_by(body: "No, it's never gone, never gone away", post: post)
 puts "Seeding finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-  
