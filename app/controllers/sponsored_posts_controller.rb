dae0f4d7c951cbecb47129d1859125308850658b
Devon Henegar <devon.henegar@gmail.com>
2016-06-15 13:05:30 -0400

    add sponsored posts

---
+++ app/controllers/sponsored_posts_controller.rb
@@ -0,0 +1,55 @@
+class SponsoredPostsController < ApplicationController
+  def show
+    @sponsoredpost = SponsoredPost.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @sponsoredpost = SponsoredPost.new
+  end
+
+  def create
+    @sponsoredpost = SponsoredPost.new
+    @sponsoredpost.title = params[:sponsored_post][:title]
+    @sponsoredpost.body = params[:sponsored_post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @sponsoredpost.topic = @topic
+
+    if @sponsoredpost.save
+      flash[:notice] = "Sponsored Post was saved successfully"
+      redirect_to [@topic,@sponsoredpost]
+    else
+      flash.now[:alert] = "There was an error saving the sponsored post. Please try again"
+      render :new
+    end
+  end
+
+  def edit
+    @sponsoredpost = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsoredpost = SponsoredPost.find(params[:id])
+    @sponsoredpost.title = params[:sponsored_post][:title]
+    @sponsoredpost.body = params[:sponsored_post][:body]
+
+    if @sponsoredpost.save
+      flash[:notice] = "Sponsored Post was updated successfully"
+      redirect_to [@sponsoredpost.topic,@sponsoredpost]
+    else
+      flash.now[:alert] = "There was an error updating the sponsored post. Please try again"
+      render :new
+    end
+  end
+  def destroy
+    @sponsoredpost = SponsoredPost.find(params[:id])
+
+    if @sponsoredpost.destroy
+      flash[:notice] = "\"#{@sponsoredpost.title}\" was completely destroyed, no refunds."
+      redirect_to @sponsoredpost.topic
+    else
+      flash.now[:alert] = "There was an error deleting the sponsored post."
+      render :show
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,15 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+
+50.times do
+  SponsoredPost.create!(
+  topic: topics.sample,
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph
+  )
+end
+
 puts "Seeding finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
