9138beb8e5fee7447175cbc0fbd1dfabd4463edc
Devon Henegar <devon.henegar@gmail.com>
2016-06-23 16:28:03 -0400

    sendign data for posts with API

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::PostsController < Api::V1::BaseController
+
+  before_action :authenticate_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
+
+
+  def update
+    post = Post.find(params[:id])
+
+    if post.update_attributes(post_params)
+      render json: post, status: 200
+    else
+      render json: {error: "post failed to update", status: 400}, status: 400
+    end
+  end
+
+  def create
+
+    topic = Topic.find(params[:topic_id])
+    post = topic.posts.build(post_params)
+    post.user = User.find_by(params[:user_id])
+    
+    if post.valid?
+      render json: post, status: 201
+    else
+      render json: {error:"post is invalid",status:400},status:400
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+
+    if post.destroy
+      render json: {message:"Post destroyed",status:200},status:200
+    else
+      render json: {message:"Post failed to be destroyed",status:400},status:400
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
